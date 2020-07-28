f2a482653f222627304176ac6e5927fc9062bf5f
cwd-k2 <kaito.augmented7th@gmail.com>
2020-07-28 23:54:33 +0900

    import directly scss

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,8 @@
 import * as React    from "react";
 import * as ReactDOM from "react-dom";
 
+import "./index.scss";
+
 class App extends React.Component {
   render() {
     return <h1>Hello World!</h1>;
